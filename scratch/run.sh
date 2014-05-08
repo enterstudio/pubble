b6ef95b9f07817b3548dbe5368771dd2a2141910
Nathan Hammond <nathan.hammond@gmail.com>
2014-05-07 17:18:01 -0700

    started work on reduced report template

--- scratch/run.sh
+++ scratch/run.sh
@@ -5,5 +5,5 @@ module load chqpoint/0.1
 module load texlive
 
 root=..
-$root/makereport.py --analysisroot $root/data --chqpointmap $root/data/chqpoint.json --htmltemplate report.html --htmldestfile out.html --pdftemplate report.tex --pdfdestfile out.pdf --debug
+$root/makereport.py --analysisroot $root/data --chqpointmap $root/data/chqpoint.json --htmltemplate kitchensink.html --htmldestfile out.html --pdftemplate report.tex --pdfdestfile out.pdf --debug
 
